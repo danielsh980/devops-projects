029342309724f684176bc15399e0f28b02d5abe4
danielsh980 <92155666+danielsh980@users.noreply.github.com>
2023-05-03 12:50:53 +0300

    Added app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+# Set up the database connection
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="mylibrary"
+)
+
+# Define a function to get the books from the database
+def get_books():
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * from books")
+    books = cursor.fetchall()
+    return books
+
+# Define a route to add a new book
+@app.route('/')
+def home():
+    books = get_books()
+    return render_template('home.html', books=books)
+
+# Define a route to add a new book
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        cursor = mydb.cursor()
+        cursor.execute("INSERT INTO books (title, author) VALUES (%s, %s)", (title, author))
+        mydb.commit()
+        return 'Books added successfully'
+    else:
+        return render_template('add.html')
+
+if __name__ == '__main__':
+    app.run()
