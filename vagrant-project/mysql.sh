3d02eb60fad3a160940712068ee7b0e56df5a17f
danielsh980 <92155666+danielsh980@users.noreply.github.com>
2023-05-03 15:12:45 +0300

    Updated mysql.sh

--- vagrant-project/mysql.sh
+++ vagrant-project/mysql.sh
@@ -12,7 +12,19 @@ cd /tmp
 git clone https://github.com/danielsh980/devops-projects.git
 
 # Create the necessary database + tables using the SQL Script
+sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
 sudo mysql -u root -p"$DATABASE_PASS" mylibrary < /tmp/devops-projects/mylibrary.sql
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+# Restart mariadb-server
+sudo systemctl restart mariadb
 
 # Starting the firewall and allowing the mariadb to access from port 3306
 sudo systemctl start firewalld
