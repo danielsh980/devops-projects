e797e9086585ab3b5764d4dfc1ff1cf0ee3e69b3
danielsh980 <92155666+danielsh980@users.noreply.github.com>
2023-05-06 08:51:52 +0300

    Updated apache.sh

--- apache.sh
+++ apache.sh
@@ -13,7 +13,7 @@ cd /tmp
 git clone -b apache-setup https://github.com/danielsh980/devops-projects.git
 sudo cp -R /tmp/devops-projects/templates /var/www/webApp
 sudo cp /tmp/devops-projects/app.py /var/www/webApp/app.py
-sudo cp /tmp/devops-projects/app.wsgi /var/www/webApp/app/wsgi
+sudo cp /tmp/devops-projects/app.wsgi /var/www/webApp/app.wsgi
 sudo cp /tmp/devops-projects/webApp.conf /etc/apache2/sites-available/webApp.conf
 sudo a2ensite webApp.conf
 sudo systemctl reload apache2
