88e054f523d1abbca27fe79d90db0c8c7a244832
danielsh980 <92155666+danielsh980@users.noreply.github.com>
2023-05-14 13:59:35 +0300

    Update schema.sql

--- Docker/MariaDB/schema.sql
+++ Docker/MariaDB/schema.sql
@@ -1,16 +1,16 @@
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';
-CREATE USER 'admin'@'%' IDENTIFIED BY 'admin123';
+CREATE USER `admin`@`localhost` IDENTIFIED BY `admin123`;
+CREATE USER `admin`@`%` IDENTIFIED BY `admin123`;
 
-DROP DATABASE IF EXISTS 'mylibrary';
-CREATE DATABASE 'mylibrary';
+DROP DATABASE IF EXISTS `mylibrary`;
+CREATE DATABASE `mylibrary`;
 
-GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'admin123';
-GRANT ALL ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin123';
+GRANT ALL ON *.* TO `admin`@`localhost` IDENTIFIED BY `admin123`;
+GRANT ALL ON *.* TO `admin`@`%` IDENTIFIED BY `admin123`;
 FLUSH PRIVILEGES;
 
 USE mylibrary;
 
-DROP TABLE IF EXISTS 'books';
+DROP TABLE IF EXISTS `books`;
 CREATE TABLE books (
     id INT(11) NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
